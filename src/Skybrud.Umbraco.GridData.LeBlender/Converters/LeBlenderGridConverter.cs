3ff687acfa14649f73293966828ca714e88f0c49
Anders Bjerner <abjerner@skybrud.dk>
2017-10-19 15:11:58 +0200

    Moslty code style, conventions etc.

--- src/Skybrud.Umbraco.GridData.LeBlender/Converters/LeBlenderGridConverter.cs
+++ src/Skybrud.Umbraco.GridData.LeBlender/Converters/LeBlenderGridConverter.cs
@@ -5,32 +5,27 @@ using Skybrud.Umbraco.GridData.LeBlender.Config;
 using Skybrud.Umbraco.GridData.LeBlender.Values;
 using Skybrud.Umbraco.GridData.Rendering;
 
-namespace Skybrud.Umbraco.GridData.LeBlender.Converters
-{
-    public class LeBlenderGridConverter : IGridConverter
-    {
+namespace Skybrud.Umbraco.GridData.LeBlender.Converters {
+
+    public class LeBlenderGridConverter : IGridConverter {
+
         /// <summary>
         /// The function that checks whether the editor is an LeBlender editor.
         /// </summary>
-        private readonly Func<GridEditor, bool> isLeBlenderEditor;
+        private readonly Func<GridEditor, bool> _isLeBlenderEditor;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LeBlenderGridConverter" /> class.
         /// </summary>
-        public LeBlenderGridConverter()
-            : this(IsLeBlenderEditor)
-        { }
+        public LeBlenderGridConverter() : this(IsLeBlenderEditor) { }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LeBlenderGridConverter" /> class.
         /// </summary>
         /// <param name="isLeBlenderEditor">The function that checks whether the editor is an LeBlender editor.</param>
         /// <exception cref="ArgumentNullException">isLeBlenderEditor</exception>
-        public LeBlenderGridConverter(Func<GridEditor, bool> isLeBlenderEditor)
-        {
-            if (isLeBlenderEditor == null) throw new ArgumentNullException(nameof(isLeBlenderEditor));
-
-            this.isLeBlenderEditor = isLeBlenderEditor;
+        public LeBlenderGridConverter(Func<GridEditor, bool> isLeBlenderEditor) {
+            _isLeBlenderEditor = isLeBlenderEditor ?? throw new ArgumentNullException(nameof(isLeBlenderEditor));
         }
 
         /// <summary>
@@ -39,15 +34,12 @@ namespace Skybrud.Umbraco.GridData.LeBlender.Converters
         /// <param name="control">A reference to the parent <see cref="GridControl" />.</param>
         /// <param name="token">The instance of <see cref="JToken" /> representing the control value.</param>
         /// <param name="value">The converted control value.</param>
-        /// <returns></returns>
-        public bool ConvertControlValue(GridControl control, JToken token, out IGridControlValue value)
-        {
+        /// <returns><code>true</code> if the converter provided a value; otherwise <code>false</code>.</returns>
+        public bool ConvertControlValue(GridControl control, JToken token, out IGridControlValue value) {
             value = null;
-            if (this.isLeBlenderEditor(control.Editor))
-            {
+            if (_isLeBlenderEditor(control.Editor)) {
                 value = GridControlLeBlenderValue.Parse(control);
             }
-
             return value != null;
         }
 
@@ -57,15 +49,12 @@ namespace Skybrud.Umbraco.GridData.LeBlender.Converters
         /// <param name="editor"></param>
         /// <param name="token">The instance of <see cref="JToken" /> representing the editor config.</param>
         /// <param name="config">The converted editor config.</param>
-        /// <returns></returns>
-        public bool ConvertEditorConfig(GridEditor editor, JToken token, out IGridEditorConfig config)
-        {
+        /// <returns><code>true</code> if the converter provided a config value; otherwise <code>false</code>.</returns>
+        public bool ConvertEditorConfig(GridEditor editor, JToken token, out IGridEditorConfig config) {
             config = null;
-            if (this.isLeBlenderEditor(editor))
-            {
+            if (_isLeBlenderEditor(editor)) {
                 config = GridEditorLeBlenderConfig.Parse(editor, token as JObject);
             }
-
             return config != null;
         }
 
@@ -74,32 +63,30 @@ namespace Skybrud.Umbraco.GridData.LeBlender.Converters
         /// </summary>
         /// <param name="control">The control to be wrapped.</param>
         /// <param name="wrapper">The wrapper.</param>
-        /// <returns></returns>
-        public bool GetControlWrapper(GridControl control, out GridControlWrapper wrapper)
-        {
+        /// <returns><code>true</code> if the converter provided a wrapper; otherwise <code>false</code>.</returns>
+        public bool GetControlWrapper(GridControl control, out GridControlWrapper wrapper) {
             wrapper = null;
-            if (this.isLeBlenderEditor(control.Editor))
-            {
+            if (_isLeBlenderEditor(control.Editor)) {
                 wrapper = control.GetControlWrapper<GridControlLeBlenderValue, GridEditorLeBlenderConfig>();
             }
-
             return wrapper != null;
         }
 
         /// <summary>
-        /// Determines whether the <paramref name="editor" /> is an LeBlender editor.
+        /// Determines whether <paramref name="editor" /> is a LeBlender editor.
         /// </summary>
         /// <param name="editor">The editor.</param>
         /// <returns>
-        ///   <c>true</c> if the <paramref name="editor" /> is an LeBlender editor; otherwise, <c>false</c>.
+        ///   <code>true</code> if <paramref name="editor" /> is a LeBlender editor; otherwise, <code>false</code>.
         /// </returns>
         /// <remarks>
-        /// Checks whether the view starts with '/App_Plugins/LeBlender/' or the alias starts with 'LeBlender.'.
+        /// Checks whether the view starts with <code>/App_Plugins/LeBlender/</code> or the alias starts with <code>LeBlender.</code>.
         /// </remarks>
-        private static bool IsLeBlenderEditor(GridEditor editor)
-        {
+        private static bool IsLeBlenderEditor(GridEditor editor) {
             return editor.View.StartsWith("/App_Plugins/LeBlender/", StringComparison.OrdinalIgnoreCase) ||
                 editor.Alias.StartsWith("LeBlender.", StringComparison.OrdinalIgnoreCase); // Kept for backwards compatibility
         }
+
     }
+
 }
